7f00086885f3a86c9231f5132fa6d44e0465d706
mfjv88 <mfjvacas@gmail.com>
2016-07-08 00:56:11 -0400

    Final revisions - Much better responsive website for mobile, new fonts, implemented flex display and last details

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,37 +1,59 @@
-$(document).ready(function(){
-  $('.type-it').typeIt({
-    strings: ['Welcome to my website'],
-    speed: 100,
-    lifeLike: true,
-    cursor: true
-  });
-  $('.parallax-window').parallax();
-  $('#menu').slicknav();
-  $(function() {
-    $('a[href*="#"]:not([href="#"])').click(function() {
-      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-        if (target.length) {
-          $('html, body').animate({
-            scrollTop: target.offset().top
-          }, 1000);
-          return false;
-        }
-      }
+$(document).ready(function() {
+    // Type it library
+    $('.type-it').typeIt({
+        strings: ['Welcome to my website'],
+        speed: 100,
+        lifeLike: true,
+        cursor: true
     });
-  });
-  $(window).scroll( function(){
-        /* Check the location of each desired element */
-        $('.hideme').each( function(i){
-            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-            /* If the object is completely visible in the window, fade it in */
-            if( bottom_of_window > bottom_of_object ){
-                $(this).animate({'opacity':'1'},1500);
+    // Parallax library
+    $('.parallax-window').parallax();
+    // Slicknav library
+    $('#menu').slicknav();
+    // Smooth scroll code
+    $(function() {
+        $('a[href*="#"]:not([href="#"])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
             }
         });
     });
-  
+    // Fade in on scroll code
+    $(window).scroll(function() {
+        /* Check the location of each desired element */
+        $('.hideme').each(function(i) {
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            /* If the object is completely visible in the window, fade it in */
+            if (bottom_of_window > bottom_of_object) {
+                $(this).animate({
+                    'opacity': '1'
+                }, 1500);
+            }
+        });
+    });
+
+    // Google Analytics Tracking code
+    (function(i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function() {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
+        a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+    ga('create', 'UA-80417262-1', 'auto');
+    ga('send', 'pageview');
 
 });
